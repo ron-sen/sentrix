0799cce8291d40590521dc27fa9fef94debe0163
ron-sen <ronsen.in02@gmail.com>
2026-05-26 00:10:42 +0530

    schema and ORM updated

--- Backend/app/models/userauth.py
+++ Backend/app/models/userauth.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Mapped , mapped_column , relationship
-from sqlalchemy import String , Text , INTEGER , ForeignKey , TIMESTAMP , func , Boolean
+from sqlalchemy import String , Text , INTEGER , ForeignKey , TIMESTAMP , func , Boolean , Date
 from app.db.connection import Base
-from datetime import datetime
+from datetime import datetime 
+from typing import Optional
 
 class User(Base):
 
@@ -16,6 +17,8 @@ class User(Base):
         return f"User(id={self.id!r} , mail={self.mail!r})"
     
     verification_tokens = relationship("VerificationToken"  , back_populates="user")
+
+    personal_profile = relationship("PersonalProfile" , back_populates="user" )
 
     # verification table
 
@@ -35,3 +38,24 @@ class VerificationToken(Base):
     user = relationship("User" , back_populates="verification_tokens")
     
 
+class PersonalProfile(Base):
+
+    __tablename__ = "personal_profile"
+
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False , primary_key=True)
+    username : Mapped[str] = mapped_column(String(50) , nullable=False ,unique=True  )
+    first_name : Mapped[Optional[str]]= mapped_column(String(100))
+    last_name : Mapped[Optional[str]] = mapped_column(String(100))
+    date_of_birth : Mapped[Optional[Date]] = mapped_column(Date , nullable=False)
+    gender : Mapped[Optional[str]] = mapped_column(String(50)) 
+    bio : Mapped[Optional[str]] = mapped_column(String)
+    profile_picture : Mapped[Optional[str]] = mapped_column(String) 
+    city : Mapped[Optional[str]] = mapped_column(String(100))
+    state : Mapped[Optional[str]] = mapped_column(String(100))
+    country : Mapped[str] = mapped_column(String(100))
+    phone_number : Mapped[str] = mapped_column(String(20))
+    phone_verified : Mapped[bool] = mapped_column(Boolean , default=False)
+    created_at : Mapped[datetime] = mapped_column(TIMESTAMP , server_default=func.current_timestamp())
+    updated_at : Mapped[datetime] = mapped_column(TIMESTAMP , server_default=func.current_timestamp() , onupdate=func.current_timestamp())
+
+    user = relationship("User" , back_populates="personal_profile")
--- Backend/app/schemas/uservalidate.py
+++ Backend/app/schemas/uservalidate.py
@@ -1,6 +1,7 @@
 
-from pydantic import BaseModel , EmailStr , Field , field_validator
-from typing import Annotated
+from pydantic import BaseModel , EmailStr , Field , field_validator , ConfigDict  , computed_field
+from typing import Annotated , Optional 
+from datetime import date 
 
 class ValidateUser(BaseModel):
 
@@ -18,6 +19,65 @@ class CreateUser(BaseModel):
     
     mail : EmailStr
     password: Annotated[str , Field(... , min_length = 8 , max_length= 72)]
+
+
+class PersonalProfileValidation(BaseModel):
+
+    username : Annotated[str , Field(..., description= "unique handle" ,min_length=3 , max_length=50)]
+    first_name : Annotated[str , Field(..., description="could be repeated" , min_length=0 , max_length=100)]
+    last_name : Optional[str] = Field(None , description="could still work" ,
+    min_length=0 , max_length=100 )
+
+    date_of_birth : date = Field(..., description="required")
+
+    gender : Optional[str] = Field(None , description="preference dependent")
+    bio : Optional[str] = Field(None , description="can be none")
+    profile_picture : Optional[str] = Field(None , description="preference  dependent")
+
+    city :  Annotated[str , Field(..., description="could be repeated" , min_length=1 , max_length=100)]
+    state :  Annotated[str , Field(..., description="could be repeated" , min_length=1 , max_length=100)]
+    country :  Annotated[str , Field(..., description="could be repeated" , min_length=1 , max_length=100)]
+    phone_number :  Annotated[str , Field(..., description="unique handle needs to be verified " , min_length=1 , max_length=20)]
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def valdiate_age(cls , dob : date):
+        today = date.today()
+        age = today.year - dob.year - ((today.month , today.day) < (dob.month , dob.day))
+
+        if age <= 10 or age >=120 :
+            raise ValueError("Age must be greater then 10 and less then 120")
+        
+        return dob
+    
+
+
+    class PersonalProfileResponse(BaseModel):
+        username : str
+        first_name : Optional[str]
+        last_name : Optional[str]
+        date_of_birth : Optional[date]
+        age : Optional[str]
+        gender : Optional[str]
+        bio : Optional[str]
+        profile_picture : Optional[str]
+        city : Optional[str]
+        state : Optional[str]
+        country : Optional[str]
+
+        model_config = ConfigDict(from_attributes = True)
+
+        @computed_field
+        @property
+        def age(self) -> Optional[int]:
+            if self.date_of_birth:
+                today = date.today()
+                return today.year - self.date_of_birth.year - (
+                    (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day)
+                )
+            return None
+
+
 
 
 
